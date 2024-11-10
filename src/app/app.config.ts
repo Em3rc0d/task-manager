e5b6a73310bcf4f3a6a5ec7139c608e2ab1147cb
farid <farid.merino1673@gmail.com>
2024-11-10 02:01:04 -0500

    app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -17,7 +17,7 @@ export const appConfig: ApplicationConfig = {
       domain: 'dev-gw3t1o22is7w2ptm.us.auth0.com',
       clientId: 'Zeq3IWP7ZdJSDoRscp8s6YIOpFrSYbHY',
       authorizationParams: {
-        redirect_uri: typeof window !== 'undefined' ? window.location.origin : "${uri}"
+        redirect_uri: typeof window !== 'undefined' ? window.location.origin : "${uri1}"
       }
     })
   ],
