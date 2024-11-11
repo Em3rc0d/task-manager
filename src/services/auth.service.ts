32853e2c97267aa5576cc937348db68d9856ce2d
farid <farid.merino1673@gmail.com>
2024-11-11 14:59:52 -0500

    sessionStorage

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,41 +5,45 @@ import { Router } from '@angular/router';
 import { HttpHeaders } from '@angular/common/http';
 
 interface usuario {
-  email: string,
-  password: string,
-  token: string
+  email: string;
+  password: string;
+  token: string;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   private apiUrl = 'https://backend-task-m.vercel.app/auth'; // Cambia esto según tu API
   private tokenKey = 'token';
-  
+
   private key = localStorage.getItem('userEmail');
   constructor(private http: HttpClient, private router: Router) {}
 
   // Login con JWT
   login(email: string, password: string): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/login`, { email, password });
-
   }
 
   // Logout: elimina el token
   logout(): void {
-    localStorage.removeItem(this.tokenKey);
+    sessionStorage.removeItem(this.tokenKey);
     this.router.navigate(['/welcome']);
+  }
+
+  // Guarda el token en sessionStorage
+  setToken(token: string): void {
+    sessionStorage.setItem(this.tokenKey, token);
   }
 
   // Verifica si el usuario está autenticado (si existe el token)
   isAuthenticated(): boolean {
-    return !!localStorage.getItem(this.tokenKey);
+    return !!sessionStorage.getItem(this.tokenKey);
   }
 
   // Obtener el token
   getToken(): string | null {
-    return localStorage.getItem(this.tokenKey);
+    return sessionStorage.getItem(this.tokenKey);
   }
 
   // Registrar usuario
